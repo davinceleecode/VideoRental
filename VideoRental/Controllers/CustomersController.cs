2c93c7c14d7d03bc5a2f9542737f02018744701d
Lee <davinceleecode>
2020-03-23 02:11:28 +0800

    implement ADD and EDIT action

--- VideoRental/Controllers/CustomersController.cs
+++ VideoRental/Controllers/CustomersController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using VideoRental.Models;
 using System.Data.Entity;
-
+using VideoRental.ViewModels;
 
 namespace VideoRental.Controllers
 {
@@ -24,7 +24,58 @@ namespace VideoRental.Controllers
         {
             _context.Dispose();
         }
-        // GET: Customers
+
+
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel()
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View("CustomerForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
+        }
+
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+
+
+
+
         public ActionResult Index()
         {
             var customer = _context.Customers.Include(c => c.MembershipType).ToList();
